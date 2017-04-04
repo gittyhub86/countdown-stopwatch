ed9226b211c4425df89fefbe8770e4f6a5f89727
Calvin Wei <cwei33@hotmail.com>
2017-04-04 21:24:47 +0000

    Add current date and time

--- modules/controllers.js
+++ modules/controllers.js
@@ -1,2 +1,8 @@
 function TimerCtrl() {
+	var now = new Date();
+	this.monthVal = now.getMonth() + 1;
+	this.yearVal = now.getFullYear();
+	this.dateVal = now.getDate();
+	this.hourVal = now.getHours();
+	this.minuteVal = 0;
 }
